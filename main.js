8027c98ba700ef7e3ae5e617921dcec1fbc4ea05
omkar775 <78637388+omkar775@users.noreply.github.com>
2021-08-13 22:22:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function bedroom()
+{
+    window.location="bed_room.html";
+}
+
+function livingroom()
+{
+    window.location="living_room.html";
+}
+
+function studyroom()
+{
+    window.location="study_room.html";
+}
+
+function kitchen()
+{
+    window.location="kitchen.html";
+}
